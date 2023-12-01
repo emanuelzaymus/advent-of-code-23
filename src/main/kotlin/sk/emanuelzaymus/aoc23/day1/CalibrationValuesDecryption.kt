182e0e06a70dcae6e1555a17fc4d56f3c926ab0a
Emanuel Zaymus <emanuel.zaymus@prosoft.sk>
2023-12-01 14:05:39 +0100

    Day 1 - Problem 2 solved

--- src/main/kotlin/sk/emanuelzaymus/aoc23/day1/CalibrationValuesDecryption.kt
+++ src/main/kotlin/sk/emanuelzaymus/aoc23/day1/CalibrationValuesDecryption.kt
@@ -2,18 +2,60 @@ package sk.emanuelzaymus.aoc23.day1
 
 import java.io.File
 
-/**
- * https://adventofcode.com/2023/day/1
- */
-fun main() {
+val digitNumberMap = mapOf(
+    "1" to 1, "2" to 2, "3" to 3, "4" to 4, "5" to 5, "6" to 6, "7" to 7, "8" to 8, "9" to 9,
+)
+
+private val wordNumberMap = mapOf(
+    "one" to 1, "two" to 2, "three" to 3, "four" to 4, "five" to 5, "six" to 6, "seven" to 7, "eight" to 8, "nine" to 9,
+)
+
+val digitAndWordNumberMap = digitNumberMap + wordNumberMap
+
+/** https://adventofcode.com/2023/day/1 */
+private fun main() {
     val lines = File("data/day1.txt").readLines()
 
-    val sum = sumOfDecryptedValues(lines)
+    val sum = sumOfDecryptedValues(lines, digitNumberMap)
+    println("Problem 1: $sum") // 55971
 
-    println(sum) // 55971
+    val sumWithWords = sumOfDecryptedValues(lines, digitAndWordNumberMap)
+    println("Problem 2: $sumWithWords") // 54719
 }
 
-fun sumOfDecryptedValues(values: List<String>): Int {
+fun sumOfDecryptedValues(lines: List<String>, numberMap: Map<String, Int>): Int {
+    return lines.sumOf {
+        it.findFirstNumber(numberMap) * 10 + it.findLastNumber(numberMap)
+    }
+}
+
+private fun String.findFirstNumber(numberMap: Map<String, Int>): Int {
+    return numberMap
+        .entries
+        .minOfWith(
+            compareBy {
+                val index = indexOf(it.key)
+                if (index == -1) length else index
+            }
+        ) {
+            it
+        }
+        .value
+}
+
+private fun String.findLastNumber(numberMap: Map<String, Int>): Int {
+    return numberMap
+        .entries
+        .maxOfWith(
+            compareBy { lastIndexOf(it.key) }
+        ) {
+            it
+        }
+        .value
+}
+
+/** First solution of Problem 1 */
+fun simpleSumOfDecryptedValues(values: List<String>): Int {
     return values
         .map {
             it.first { c -> c.isDigit() } to it.last { c -> c.isDigit() }
--- src/test/kotlin/sk/emanuelzaymus/aoc23/day1/CalibrationValuesDecryptionTest.kt
+++ src/test/kotlin/sk/emanuelzaymus/aoc23/day1/CalibrationValuesDecryptionTest.kt
@@ -6,8 +6,8 @@ import kotlin.test.assertEquals
 class CalibrationValuesDecryptionTest {
 
     @Test
-    fun sumOfDecryptedValues() {
-        val actual = sumOfDecryptedValues(
+    fun `simpleSumOfDecryptedValues - only digits - should return correct result`() {
+        val actual = simpleSumOfDecryptedValues(
             listOf(
                 "1abc2",
                 "pqr3stu8vwx",
@@ -17,6 +17,39 @@ class CalibrationValuesDecryptionTest {
         )
 
         assertEquals(142, actual)
+    }
+
+    @Test
+    fun `sumOfDecryptedValues - only digits - should return correct result`() {
+        val actual = sumOfDecryptedValues(
+            listOf(
+                "1abc2",
+                "pqr3stu8vwx",
+                "a1b2c3d4e5f",
+                "treb7uchet",
+            ),
+            digitNumberMap
+        )
+
+        assertEquals(142, actual)
+    }
+
+    @Test
+    fun `sumOfDecryptedValues - digits with spelled numbers - should return correct result`() {
+        val actual = sumOfDecryptedValues(
+            listOf(
+                "two1nine",
+                "eightwothree",
+                "abcone2threexyz",
+                "xtwone3four",
+                "4nineeightseven2",
+                "zoneight234",
+                "7pqrstsixteen",
+            ),
+            digitAndWordNumberMap
+        )
+
+        assertEquals(281, actual)
     }
 
 }
