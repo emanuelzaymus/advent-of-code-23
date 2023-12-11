29899d7a3f25ff042204b11b7fa816bf76a3bc63
Emanuel Zaymus <emanuel.zaymus@prosoft.sk>
2023-12-11 09:57:36 +0100

    Maze extracted into extra file

---
+++ src/main/kotlin/sk/emanuelzaymus/aoc23/day10/Maze.kt
@@ -0,0 +1,47 @@
+package sk.emanuelzaymus.aoc23.day10
+
+typealias Maze = Array<Array<Position>>
+
+fun Maze.findStartPosition(): Position {
+    forEach { row ->
+        row.forEach { position ->
+            if (position.tile == Tile.START) {
+                return position
+            }
+        }
+    }
+
+    throw IllegalStateException("Maze does not contain start position.")
+}
+
+fun Maze.findNextPosition(currentPosition: Position, lastPosition: Position?): Position {
+    for (direction in Direction.entries) {
+        val nextPosition = getPositionTo(direction, currentPosition)
+
+        if (
+            nextPosition != null
+            && currentPosition.isConnectingTo(nextPosition)
+            && nextPosition != lastPosition
+        ) {
+            return nextPosition
+        }
+    }
+
+    throw IllegalStateException("No next position found from current position: $currentPosition.")
+}
+
+private fun Maze.getPositionTo(direction: Direction, fromPosition: Position): Position? {
+    val newX = direction.shiftX(fromPosition.x)
+    val newY = direction.shiftY(fromPosition.y)
+
+    if (!isPositionInside(newX, newY)) {
+        return null
+    }
+
+    return this[newX][newY]
+}
+
+private fun Maze.isPositionInside(x: Int, y: Int): Boolean {
+    return x in indices
+        && y in 0..<this[x].size
+}
--- src/main/kotlin/sk/emanuelzaymus/aoc23/day10/MazeProblems.kt
+++ src/main/kotlin/sk/emanuelzaymus/aoc23/day10/MazeProblems.kt
@@ -1,9 +1,6 @@
 package sk.emanuelzaymus.aoc23.day10
 
-import sk.emanuelzaymus.aoc23.day10.Tile.*
 import java.io.File
-
-private typealias Maze = Array<Array<Position>>
 
 private fun main() {
     val inputMaze = File("data/day10.txt").readText()
@@ -17,20 +14,16 @@ private fun main() {
     println("Problem 2: $numberOfEnclosingTiles") //
 }
 
-fun findNumberOfEnclosedTiles(inputMaze: String): Int {
-    TODO("Not yet implemented")
-}
-
 fun findNumberOfStepsFromStartToFarthestPoint(inputMaze: String): Int {
     val maze = readMaze(inputMaze)
 
-    val startPosition = findStartPosition(maze)
+    val startPosition = maze.findStartPosition()
     var currentPosition = startPosition
     var lastPosition: Position? = null
     var stepCount = 0
 
     while (true) {
-        val newPosition = findNextPosition(maze, currentPosition, lastPosition)
+        val newPosition = maze.findNextPosition(currentPosition, lastPosition)
         lastPosition = currentPosition
         currentPosition = newPosition
 
@@ -45,48 +38,8 @@ fun findNumberOfStepsFromStartToFarthestPoint(inputMaze: String): Int {
     return stepCount / 2
 }
 
-private fun findNextPosition(maze: Maze, currentPosition: Position, lastPosition: Position?): Position {
-    for (direction in Direction.entries) {
-        val nextPosition = maze.getPositionTo(direction, currentPosition)
-
-        if (
-            nextPosition != null
-            && currentPosition.isConnectingTo(nextPosition)
-            && nextPosition != lastPosition
-        ) {
-            return nextPosition
-        }
-    }
-
-    throw IllegalStateException("No next position found from current position: $currentPosition.")
-}
-
-private fun Maze.getPositionTo(direction: Direction, fromPosition: Position): Position? {
-    val newX = direction.shiftX(fromPosition.x)
-    val newY = direction.shiftY(fromPosition.y)
-
-    if (!isPositionInside(newX, newY)) {
-        return null
-    }
-
-    return this[newX][newY]
-}
-
-private fun Maze.isPositionInside(x: Int, y: Int): Boolean {
-    return x in indices
-        && y in 0..<this[x].size
-}
-
-private fun findStartPosition(maze: Maze): Position {
-    maze.forEach { row ->
-        row.forEach { position ->
-            if (position.tile == START) {
-                return position
-            }
-        }
-    }
-
-    throw IllegalStateException("Maze does not contain start position.")
+fun findNumberOfEnclosedTiles(inputMaze: String): Int {
+    TODO("Not yet implemented")
 }
 
 private fun readMaze(inputMaze: String): Maze {
