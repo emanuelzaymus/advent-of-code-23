ebfdb993a11e544fc12eb5d9e37dd40c342fd4a4
Emanuel Zaymus <emanuel.zaymus@prosoft.sk>
2023-12-15 22:31:58 +0100

    Day 14 - part 2 - done

--- src/main/kotlin/sk/emanuelzaymus/aoc23/day14/PlatformLoad.kt
+++ src/main/kotlin/sk/emanuelzaymus/aoc23/day14/PlatformLoad.kt
@@ -13,9 +13,9 @@ private fun main() {
 
     println("Problem 1: $platformLoad") // 106186
 
-    val platformLoadAfterCycles = calculateTiltedPlatformLoad(platformInput, 10_000)
+    val platformLoadAfterCycles = calculateTiltedPlatformLoad(platformInput, 1000_000_000)
 
-    println("Problem 2: $platformLoadAfterCycles") // 106350 is too low !!!
+    println("Problem 2: $platformLoadAfterCycles") // 106390
 }
 
 fun calculateTiltedNorthPlatformLoad(platformInput: String): Int {
@@ -29,11 +29,89 @@ fun calculateTiltedNorthPlatformLoad(platformInput: String): Int {
 fun calculateTiltedPlatformLoad(platformInput: String, cycles: Int): Int {
     val platform = readPlatform(platformInput)
 
+    if (cycles < 10_000) {
+        calculateUsingBruteForce(platform, cycles)
+    }
+
+    return calculateUsingStatistics(platform, cycles)
+}
+
+private fun calculateUsingBruteForce(platform: Platform, cycles: Int): Int {
     repeat(cycles) {
         platform.tiltWholeCycle()
     }
 
     return platform.calculatePlatformLoad()
+}
+
+private fun calculateUsingStatistics(
+    platform: Platform,
+    totalCycles: Int,
+    warmUpCycles: Int = 10_000,
+    statisticsCycles: Int = 100,
+): Int {
+    val statistics = collectStatisticsForCycles(platform, warmUpCycles, statisticsCycles)
+
+    val period = findPeriod(statistics)
+
+    val reminder = (totalCycles - warmUpCycles - 1) % period
+
+    return statistics[reminder]
+}
+
+private fun collectStatisticsForCycles(platform: Platform, warmUpCycles: Int, statisticsCycles: Int): List<Int> {
+    repeat(warmUpCycles) {
+        platform.tiltWholeCycle()
+    }
+
+    return (0..<statisticsCycles)
+        .map {
+            platform.tiltWholeCycle()
+            platform.calculatePlatformLoad()
+        }
+        .toList()
+}
+
+private fun findPeriod(statistics: List<Int>): Int {
+    for ((numberIndex, number) in statistics.withIndex()) {
+        val indexInSlice = statistics
+            .slice(numberIndex + 1..<statistics.size)
+            .indexOf(number)
+
+        if (indexInSlice == -1) {
+            continue
+        }
+
+        val realIndexOfNextOccurrence = numberIndex + 1 + indexInSlice
+
+        val possiblePeriod = realIndexOfNextOccurrence - numberIndex
+
+        if (isPeriod(statistics, possiblePeriod)) {
+            return possiblePeriod
+        }
+    }
+    error("Period not found.")
+}
+
+private fun isPeriod(statistics: List<Int>, possiblePeriod: Int): Boolean {
+    check(statistics.size >= possiblePeriod * 2) { "Statistics size must be at least 2 times the possible period." }
+
+    for ((index, number) in statistics.withIndex()) {
+        if (index >= possiblePeriod) {
+            break
+        }
+
+        var nextOccurrence = index + possiblePeriod
+
+        while (nextOccurrence < statistics.size) {
+            if (statistics[nextOccurrence] != number) {
+                return false
+            }
+
+            nextOccurrence += possiblePeriod
+        }
+    }
+    return true
 }
 
 fun readPlatform(platformInput: String): Platform {
